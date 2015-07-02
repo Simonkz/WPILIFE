2e6ae7afadfc6cdafac427e876a27440e0fec656
AgentKWang <han0122@gmail.com>
2015-07-01 23:30:16 -0400

    add resend email page layout

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -4,15 +4,15 @@ class Account extends CI_Controller
     function __construct()	{
         parent::__construct();
         $this->load->library('activatelib');
-        $this->load->library('userlib');
+        $this->load->library('Users');
     }
 
-    function index($uid){
+    function resendemail($uid){
         $data['title'] = 'Activation';
         $data['info'] = "Bad Request";
         $link = $this->activatelib->get_link_by_uid($uid);
         if(strlen($link) == 32){
-            $user = $this->userlib->get_user_info($uid);
+            $user = $this->users->get_user_info($uid);
             if($user){
                 $this->activatelib->send_activation_email($user['users_email_address'], $link);
             }
--- application/views/Account/resend_activate_email.php
+++ application/views/Account/resend_activate_email.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>
+	<title>Activation</title>
 <?php $this->load->view('includes/import');?>
 <link rel="stylesheet" href="<?php echo base_url(); ?>css/signup.css" type="text/css" />
 </head>
@@ -10,12 +10,19 @@
 <!-- Wrapper / Start -->
 <div id="wrapper">
     <?php $this->load->view('includes/header');?>
-    <div class="col-md-5">
-        <h4>Your account is not activated!</h4>
-        <p>An E-mail contains activate link has been sent to you E-mail,
-            if you did not receive, please try to find in your <b>junk folder</b> or
-            <a href="/account/resendemail?uid=<?=$uid?>">Resend Activated E-mail</a>
-        </p>
+    <div class="container">
+        <div class="six columns">
+            <h4>Your account is not activated!</h4>
+            <p>An E-mail contains activate link has been sent to you E-mail,
+                if you did not receive, please try to find in your <strong>junk folder</strong> or
+                <a href="/account/resendemail/<?=$uid?>">Resend Activated E-mail</a>
+            </p>
+        </div>
+        <div class="six columns">
+            <h4>您的账户还未激活！</h4>
+            <p>请检查收件箱，如果未收到请在<strong>垃圾邮件</strong>中查找 建议使用 Gmail， Hotmail  部分国内邮件服务商可能无法收到激活邮件。<br>
+            <a href="/account/resendemail/<?=$uid?>">重发激活邮件</a></p>
+        </div>
     </div>
 </div>
 <?php $this->load->view('includes/footer');?>
