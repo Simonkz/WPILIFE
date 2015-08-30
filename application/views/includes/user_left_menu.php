00b50d7e8e555e2a249407dc65bd9fb718bf8d3d
Hao Zhou <i@hzhou.me>
2015-08-30 13:01:52 -0400

    Update user_left_menu.php

--- application/views/includes/user_left_menu.php
+++ application/views/includes/user_left_menu.php
@@ -19,7 +19,6 @@
 				</ul>
 
 				<ul class="contact-informations second">
-					<li><i class="halflings headphones"></i> <p>+1 508-335-9815</p></li>
 					<li><i class="halflings envelope"></i> <p><a href="mailto:wpilife@gmail.com">wpilife@gmail.com</a></p></li>
 					<li><i class="halflings globe"></i> <p><a href="http://sbzhouhao.net/">http://sbzhouhao.net/</a></p></li>
 				</ul>
